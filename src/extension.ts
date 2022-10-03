48ef64e1629dc5276f2c4cc3a6c786d728399688
Bernhard <b_walter@arcor.de>
2022-10-03 15:27:27 +0200

    add command to install python module

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,9 @@ import { CadqueryViewer } from './viewer';
 import { CadqueryController } from "./controller";
 import { version as cq_vscode_version } from "./version";
 
+const URL =
+    "https://github.com/bernhard-42/vscode-cadquery-viewer/releases/download";
+
 export function activate(context: vscode.ExtensionContext) {
 
 	//	Commands
@@ -15,8 +18,24 @@ export function activate(context: vscode.ExtensionContext) {
 			if (editor !== undefined){
 				vscode.window.showTextDocument(editor, column);
 			}
-		})
-	);
+            }
+        )
+    );
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand(
+            "cadquery-viewer.installPythonModule",
+            () => {
+                const terminal = vscode.window.createTerminal(
+                    "Cadquery Viewer installation"
+                );
+                terminal.show();
+                terminal.sendText(
+                    `pip install ${URL}/v${cq_vscode_version}/cq_vscode-${cq_vscode_version}-py3-none-any.whl`
+                );
+            }
+        )
+    );
 
 	//	Register Web view
 
