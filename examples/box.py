86fd80df163e5b9c023a0a7231adfee106e79639
Bernhard <b_walter@arcor.de>
2022-10-03 17:11:14 +0200

    added ortho parameter

--- examples/box.py
+++ examples/box.py
@@ -2,7 +2,7 @@ import cadquery as cq
 from cq_vscode import show_object, reset_show, set_defaults
 
 reset_show()
-set_defaults(axes=True, transparent=False, collapse=1)
+set_defaults(axes=True, transparent=False, collapse=1, grid=(True, True, True))
 
 box = cq.Workplane().box(1, 2, 1).edges().chamfer(0.4)
 show_object(box, name="box", options={"alpha": 0.5})
@@ -17,4 +17,5 @@ show_object(
     # three-cad-viewer args
     collapse="1",
     reset_camera=False,
+    ortho=False,
 )
