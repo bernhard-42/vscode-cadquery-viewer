5fe932d7f8dd78e5b8a19dd3e92d90b22b38beaf
Bernhard <b_walter@arcor.de>
2022-10-03 10:52:59 +0200

    enhance examples

--- examples/box.py
+++ examples/box.py
@@ -1,7 +1,20 @@
 import cadquery as cq
-from cq_vscode import show, set_defaults, reset_defaults
+from cq_vscode import show_object, reset_show, set_defaults
 
-set_defaults(axes=True, transparent=True)
+reset_show()
+set_defaults(axes=True, transparent=False, collapse=1)
 
-box = cq.Workplane().box(1, 1, 1).edges().chamfer(0.1)
-show(box, reset_camera=True)
+box = cq.Workplane().box(1, 2, 1).edges().chamfer(0.4)
+show_object(box, name="box", options={"alpha": 0.5})
+
+sphere = cq.Workplane().sphere(0.6)
+
+show_object(
+    sphere,
+    # show_object args
+    "sphere",
+    {"color": (10, 100, 110)},
+    # three-cad-viewer args
+    collapse="1",
+    reset_camera=False,
+)
