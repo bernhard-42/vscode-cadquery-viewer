65eee909897c3770ffe48d8c34c0ada1053ff6f8
Bernhard <b_walter@arcor.de>
2022-10-03 15:29:27 +0200

    add some more functions for defaults handling

--- cq_vscode/__init__.py
+++ cq_vscode/__init__.py
@@ -1,2 +1,2 @@
 from .show import show, show_object, reset_show
-from jupyter_cadquery import set_defaults, reset_defaults
+from jupyter_cadquery import set_defaults, reset_defaults, get_defaults, get_default
